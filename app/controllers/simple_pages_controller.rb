2ab14d66a49e153e80d7de1c765e1ba5b84d2866
louie <l.e.ormston@gmail.com>
2019-02-15 21:32:05 +0000

    Contact page complete

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,16 @@
 class SimplePagesController < ApplicationController
   def index
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @phone = params[:phone]
+    @message = params[:message]
+    @messagebody = "Name: #{@name}\n" + "Phone: #{@phone}\n" + "Email: #{@email}\n" + "Message: #{@message}"
+    ActionMailer::Base.mail(from: @email,
+      to: 'sales@a1directsupplies.co.uk',
+      subject: "A new contact form message from #{@name}",
+      body: @messagebody).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/products'
+  post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
