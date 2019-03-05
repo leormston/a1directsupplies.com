7ad1352ccd4398349e292437bb06648a5c598a04
louie <l.e.ormston@gmail.com>
2019-03-05 17:23:35 +0000

    ProductController updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -81,6 +81,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :part_number, :image_url, :price, :category, :brand)
+      params.require(:product).permit(:name, :description, :part_number, :image_url, :price, :category, :brand, :display)
     end
 end
---
+++ db/migrate/20190305172103_add_image_column_to_products_table.rb
@@ -0,0 +1,5 @@
+class AddImageColumnToProductsTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :image_url, :string
+  end
+end
