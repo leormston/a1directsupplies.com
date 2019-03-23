01d6f8ef523f9cd28dde1d9fb2eb98e40fafa192
louie <l.e.ormston@gmail.com>
2019-03-23 10:59:43 +0000

    Stripe is implemented correctly

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -53,18 +53,46 @@ class CartsController < ApplicationController
   end
 
   def checkout
-    @carts = Cart.where(order_id: 0, user_id: current_user.id)
-    @total = Cart.where(user_id: current_user.id, order_id: 0).sum('subtotal')
-    @order = Order.create(user_id: current_user.id, Street: current_user.streetname, City: current_user.city,
-    Postcode: current_user.postcode, total: @total)
-    Cart.where(order_id: 0, user_id: current_user.id).update_all(order_id: @order.id)
-    @orders = Order.where(user_id: current_user.id)
+    token = params[:stripeToken]
+    @carts = Cart.where(order_id: 0, user_id: current_user.id) #this checks all orders which haven't been paid for and belong to the current user logged in
+    @total = Cart.where(user_id: current_user.id, order_id: 0).sum('subtotal') #finds this total value of the transaction by adding all of the subtotals up
 
-    # for the confirmation email
-    @carts = Cart.where(order_id: @order.id)
-    @user = current_user
-    @products = Product.all
-    UserMailer.purchase_confirmation(@user.email, @carts, @total, @order, @user, @products).deliver_now
+    begin
+
+      charge = Stripe::Charge.create(
+        amount: @total.to_i,
+        currency: "gbp",
+        source: token,
+        description: 'Payment Processed',
+        receipt_email: params[:stripeEmail]
+      )
+
+      if charge.paid #this is what happens if the payment is succesful
+        @order = Order.create(user_id: current_user.id, Street: current_user.streetname, City: current_user.city,
+        Postcode: current_user.postcode, total: @total) #creates order
+        Cart.where(order_id: 0, user_id: current_user.id).update_all(order_id: @order.id) #updates rows in cart table with the correct order_id
+        flash[:notice] = "Payment Succesful"
+        flash[:success] = "Order (Number 000#{@order.id}) is being processed and dealt with"
+        # for the confirmation email
+        @carts = Cart.where(order_id: @order.id)
+        @user = current_user
+        @products = Product.all
+        UserMailer.purchase_confirmation(@user.email, @carts, @total, @order, @user, @products).deliver_now
+
+      end
+
+    rescue Stripe::CardError => e
+      #The card has been declined
+      redirect_to(payments_show_path)
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+
+    end
+
+    # @orders = Order.where(user_id: current_user.id)
+
+
   end
 
 end
